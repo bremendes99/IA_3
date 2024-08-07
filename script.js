4985d765ce0e7ac77bef7cfa1d8b14a532c9a69d
Brenda Mendes <125170004+bremendes99@users.noreply.github.com>
2024-08-07 10:40:29 -0300

    perguntas

--- script.js
+++ script.js
@@ -6,17 +6,24 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
+        enunciado: "Quais são as principais causas das enchentes urbanas?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "A impermeabilização do solo devido à construção de estradas e prédios impede que a água da chuva seja absorvida, levando ao acúmulo de água nas ruas e a enchentes.",
+            "A falta de manutenção e a obstrução das redes de drenagem urbana podem causar o transbordamento de córregos e bueiros, resultando em enchentes nas áreas urbanas."
         ]
     },
     {
-        enunciado: "Pergunta 2",
+        enunciado: "Como as enchentes afetam a infraestrutura de uma cidade?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "Enchentes podem danificar estradas, pontes e edifícios, resultando em altos custos de reparo e interrupções significativas nos serviços e transporte.",
+            "Além dos danos físicos, enchentes podem comprometer o fornecimento de água e energia, causar problemas em sistemas de esgoto e gerar riscos à saúde pública devido ao contágio de doenças."
         ]
-    }
+    },
+    {
+        enunciado: "Quais são algumas medidas eficazes para prevenir enchentes?",
+        alternativas: [
+            "A construção de sistemas de drenagem eficientes e a implementação de soluções baseadas na natureza, como parques e áreas verdes, podem ajudar a reduzir o risco de enchentes.",
+            "A educação pública sobre práticas de conservação e o controle do uso do solo para evitar a impermeabilização excessiva são estratégias importantes para a prevenção de enchentes."
+        ]
+    },
 ];
